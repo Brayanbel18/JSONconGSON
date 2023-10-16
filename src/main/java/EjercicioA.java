01e81dcc3282e82ae6e4f00357eebe09f1fac031
BrayanBeltran18 <brayanbel18@gmail.com>
2023-10-15 23:17:16 -0700

    Correción EjercicioA

--- src/main/java/EjercicioA.java
+++ src/main/java/EjercicioA.java
@@ -1,50 +1,45 @@
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-
-import java.io.FileReader;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class EjercicioA {
     public static void main(String[] args) {
         try {
-            // Analizar el archivo JSON.
-            JSONParser parser = new JSONParser();
-            // Leer archivo y almacenar en JSONArray.
-            JSONArray jsonArray = (JSONArray) parser.parse(new FileReader("src\\car_sales.json"));
+            // Crear un ObjectMapper para manejar JSON.
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            // Leer el archivo JSON.
+            JsonNode jsonNode = objectMapper.readTree(new File("src\\car_sales.json"));
 
             // Maps para almacenar datos.
             Map<String, Double> preciosPorMarca = new HashMap<>();
             Map<String, Integer> carrosPorMarca = new HashMap<>();
 
-            for (Object object : jsonArray) {
-                // Obtener los datos del carro de cada objeto.
-                JSONObject datosCarro = (JSONObject) object;
-                String marca = (String) datosCarro.get("car");
-                // Extraer el precio.
-                double precio = Double.parseDouble(((String) datosCarro.get("price")).substring(1));
+            for (JsonNode elemento : jsonNode) {
+                // Obtener la marca del carro de cada elemento.
+                String marca = elemento.get("car").asText();
+                // Obtener el precio y convertirlo a un número decimal.
+                double precio = Double.parseDouble(elemento.get("price").asText().substring(1));
 
-                // Actualizar maps con información por marca de carro.
+                // Actualizar los maps con información por marca de carro.
                 preciosPorMarca.put(marca, preciosPorMarca.getOrDefault(marca, 0.0) + precio);
                 carrosPorMarca.put(marca, carrosPorMarca.getOrDefault(marca, 0) + 1);
             }
 
-            // Imprimir el reporte.
+            // Imprimir el reporte de precio promedio por marca.
             System.out.println("Precio promedio por marca de carro:");
-            for (String car : preciosPorMarca.keySet()) {
-                double precioTotal = preciosPorMarca.get(car);
-                int contador = carrosPorMarca.get(car);
+            for (String marca : preciosPorMarca.keySet()) {
+                double precioTotal = preciosPorMarca.get(marca);
+                int contador = carrosPorMarca.get(marca);
                 double precioPromedio = precioTotal / contador;
-                System.out.println(car + ": $" + precioPromedio);
+                System.out.println(marca + ": $" + precioPromedio);
             }
 
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
-
-
-
-
